3c15c8262d7ba74cc9a2ae8c012b14cd02c3adf2
Havret <h4vret@gmail.com>
2021-04-08 20:52:08 +0200

    init

---
+++ ConsumerA/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ActiveMQ.Artemis.Client;
+using ActiveMQ.Artemis.Client.AutoRecovering.RecoveryPolicy;
+using ActiveMQ.Artemis.Client.Exceptions;
+using ActiveMQ.Artemis.Client.Transactions;
+using Microsoft.Extensions.Logging;
+
+namespace ConsumerA
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, _) => { cts.Cancel(); };
+
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Information);
+                builder.AddConsole();
+            });
+
+            var connectionFactory = new ConnectionFactory
+            {
+                AutomaticRecoveryEnabled = true,
+                RecoveryPolicy = RecoveryPolicyFactory.ConstantBackoff(TimeSpan.FromSeconds(5), retryCount: 5),
+                LoggerFactory = loggerFactory
+            };
+            var endpoint = Endpoint.Create(host: "localhost", port: 5672, "guest", "guest");
+
+            await using var connection = await connectionFactory.CreateAsync(endpoint, cts.Token);
+            connection.ConnectionRecoveryError += (_, _) =>
+            {
+                Console.WriteLine("Disconnected");
+                cts.Cancel();
+            };
+
+            var address = "my-address";
+            var queue = "my-queue1";
+
+            var topologyManager = await connection.CreateTopologyManagerAsync(cts.Token);
+            await topologyManager.DeclareQueueAsync(new QueueConfiguration
+            {
+                Address = address,
+                Name = queue,
+                AutoCreateAddress = true,
+                RoutingType = RoutingType.Multicast,
+                Exclusive = true,
+                Durable = true,
+            }, cts.Token);
+            await topologyManager.DisposeAsync();
+
+            var consumer = await connection.CreateConsumerAsync(address, queue, cancellationToken: cts.Token);
+
+            Console.WriteLine($"Attached to queue: {queue}");
+
+            await Task.Run(async () =>
+            {
+                while (!cts.IsCancellationRequested)
+                {
+                    try
+                    {
+                        await using var transaction = new Transaction();
+                        var msg = await consumer.ReceiveAsync(cts.Token);
+                        await consumer.AcceptAsync(msg, transaction, cts.Token);
+                        await transaction.CommitAsync(cts.Token);
+                        Console.WriteLine($"Received message: {msg.GetBody<string>()}");
+                    }
+                    catch (OperationCanceledException)
+                    {
+                    }
+                    catch (ActiveMQArtemisClientException e)
+                    {
+                        Console.Error.WriteLine(e);
+                    }
+                }
+            }, cts.Token);
+        }
+    }
+}
